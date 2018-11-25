9097b544b87782ad51fd6ce697df148cd110997f
candyrobot <findwkwk@gmail.com>
2018-11-26 01:59:07 +0900

    #1

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,6 +8,10 @@ class ImagesController < ApplicationController
   end
 
   def create
+    # 重複チェック
+    # image = Image.where url: params[:url]
+    # return if image.length != 0
+
     image = Image.new({url: params[:url] })
     image.save
   end
