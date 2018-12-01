65c935a4293e7f456e9f5b0f812cdea7585a62a8
Hideaki Tiánzhōng <rivalknockout@gmail.com>
2018-12-01 21:10:21 +0900

    #29 やっとwebpackでreactのソースを /app/assets/javascripts/webpack/app.js に書き出すことができた。。
    - cd front
    - npm run build // sudo 必要なし

---
+++ front/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  entry: { app: './src/index.js' },
+  output: {
+    // path: '/app/assets/javascripts/webpack',
+    path: path.join(__dirname, "../app/assets/javascripts/webpack/"),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // babel-loaderを使って変換する
+          options: {
+            presets: ['@babel/env', '@babel/react'], // env presetでES2015から変換、react presetでReactのJSX文法を変換
+          }
+        }
+        // query: {
+        //   presets: ['es2015', 'react'],
+        // },
+      },
+      {
+          test: /\.svg$/,
+          loader: 'svg-inline-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules']
+      }
+    ]
+  },
+}
